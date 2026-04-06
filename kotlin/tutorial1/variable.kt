a3af5ba09bb79255a9cf191805d00734a2d01b3e
Mwansambe <jacobmwansambe1@gmail.com>
2026-04-06 17:38:36 +0300

    practise kotlin input

---
+++ kotlin/tutorial1/variable.kt
@@ -0,0 +1,15 @@
+fun main()
+{
+    println("Welcome to the word of programing with koltlin")
+    val a: Int = 3;
+    val b: Int = 5;
+    println("the sum of the two numbers is: ${a + b}")
+println("${a == b}")
+
+println("Entet the number of your choice")
+val input = readln()
+println(input)
+
+
+
+}
