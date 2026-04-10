25254b97fb5577faeb7d632024c7bc8a2ddcdfd3
Mwansambe <jacobmwansambe1@gmail.com>
2026-04-10 09:55:19 +0300

    today workOut

---
+++ kotlin/tutorial1/input.kt
@@ -0,0 +1,6 @@
+fun main(){
+    println("Enter a number")
+    var input = readln()
+    println("you have entered a number $input")
+    
+}
---
+++ kotlin/tutorial1/pr.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+    var a = 2
+    var b = 7
+    println("the value of a is $a")
+    println("the value of b is $b")
+
+    var BothEven = a % 2 == 0 && b % 2 == 0
+   println("Are they both even numbers $BothEven")
+
+}
