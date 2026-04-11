46997aa38d28b72fa39afe41ca17f3d6ecfea171
Mwansambe <jacobmwansambe1@gmail.com>
2026-04-11 19:38:29 +0300

    this is evening

---
+++ kotlin/tutorial1/Control.kt
@@ -0,0 +1,10 @@
+fun main(){
+    println("Enter the amount of number you want to enter")
+
+    var number = readln().toInt()
+    var start = 0
+    while(start < number){
+        start++
+        println(start)
+    }
+}
--- kotlin/tutorial1/input.kt
+++ kotlin/tutorial1/input.kt
@@ -1,6 +1,9 @@
 fun main(){
     println("Enter a number")
     var input = readln()
-    println("you have entered a number $input")
+    var Number = input.toIntOrNull()
+
+
+    println("you have entered a number $Number")
     
 }
